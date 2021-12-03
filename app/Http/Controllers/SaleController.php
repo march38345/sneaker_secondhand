a495e52ae6f7c8663afb7221adb507fe7a9f22a6
march38345 <martz123456@hotmail.com>
2021-12-03 23:49:08 +0700

    up date sale controller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -11,105 +11,67 @@ use PhpOption\Option;
 
 class SaleController extends Controller
 {
- public function set_for_sale()
- {   
-    $province = Provinces::get();
-     return view('set_for_sale')->with(compact('province'));
- }
- public function confirmsale(Request $request)
- {
-     
-    $request->validate(
-        ['description'=>'required',
-         'price'      =>'required|max:255',
-         'img'        =>'required'
-        ],
-        [
-         'description.required' =>"กรุณากรอกข้อมูล",
-         'price.required' =>"กรุณากรอกราคาของสินค้า",
-         
-         'price.max' =>"ห้ามป้อนข้อมูลเกิน255ตัวอักษร",
-        'img.required'=>"กรุณาอัปโหลดรูปภาพของสินค้าที่ต้องการขาย",
-        'img.mimes' => "กรุณาเลือกไฟล์ .jpeg ,.jpg ,.png"
+    public function set_for_sale()
+    {
+        $province = Provinces::get();
+        return view('set_for_sale')->with(compact('province'));
+    }
+    public function confirmsale(Request $request)
+    {
 
-        ]
-        
-    );
-    #จัดการรูปภาพ
-    $obj = $request->img;
-    $path = "";
-    $imgname = "";
-    $i = 0;
-    $len = count($obj);
-    foreach($obj as $c){
-        $imgname = hexdec(uniqid()).".".strtolower($c->getClientOriginalExtension());
-      
-        $c->move('image/store/',$imgname);#uploads รูปภาพไปที่public/store/
-        #checkตัววสุดท้าย
-        if($i==$len-1){
-            $path .= $imgname;
-        }else{
-            $path .= $imgname.",";
-        }
-       $i++;
+        $request->validate(
+            [
+                'description' => 'required',
+                'price'      => 'required|max:255',
+                'img'        => 'required'
+            ],
+            [
+                'description.required' => "กรุณากรอกข้อมูล",
+                'price.required' => "กรุณากรอกราคาของสินค้า",
+
+                'price.max' => "ห้ามป้อนข้อมูลเกิน255ตัวอักษร",
+                'img.required' => "กรุณาอัปโหลดรูปภาพของสินค้าที่ต้องการขาย",
+                'img.mimes' => "กรุณาเลือกไฟล์ .jpeg ,.jpg ,.png"
+
+            ]
+
+        );
+        #จัดการรูปภาพ
+        $obj = $request->img;
+        $path = "";
         $imgname = "";
-        
+        $i = 0;
+        $len = count($obj);
+        foreach ($obj as $c) {
+            $imgname = hexdec(uniqid()) . "." . strtolower($c->getClientOriginalExtension());
+
+            $c->move('image/store/', $imgname); #uploads รูปภาพไปที่public/store/
+            #checkตัววสุดท้าย
+            if ($i == $len - 1) {
+                $path .= $imgname;
+            } else {
+                $path .= $imgname . ",";
+            }
+            $i++;
+            $imgname = "";
+        }
+        $province = Provinces::get()->where('id', $request->province);
+        foreach ($province as $item) {
+            $name_province = $item->name_in_thai;
+        }
+        dd($province);
     }
-    $province = Provinces::get()->where('id',$request->province);
-    foreach($province as $item){
-         $name_province = $item->name_in_thai;
+    public function district(Request $request)
+    {
+        $district = Districts::get()->where('province_id', $request->select);
+
+        $result = array();
+        $output = '<option value="">กรุณาเลือกอำเภอ</option>';
+        foreach ($district as $item) {
+
+            $output .= '<option value="' . $item->id . '">' . $item->name_in_thai . '</option>';
+        }
+
+        echo $output;
     }
-  
-    
-    $district = Districts::get()->where('id',$request->district);
-    foreach($district as $item){
-        $name_district = $item->name_in_thai;
-   }
-    
-    
-     
-    $statement = new Statement;
-    $statement->user_id = Auth::user()->id;
-    $statement->name = Auth::user()->name;
-    $statement->brand_name = $request->brand_name;
-    $statement->path_img =$path;
-    $statement->description = $request->description;
-    $statement->size = $request->size;
-    $statement->price = $request->price;
-    $statement->phone = $request->phone;
-    $statement->status = 0;
-   $statement->province = $name_province;
-    $statement->district = $name_district;
-
-
-   
- 
-    $state =  $statement->save();
-    if(!$state){
-      return  redirect()->back()->with('error','ตั้งขายไม่สำเร็จ');
-    } 
-    return  redirect()->back()->with('success','ตั้งการขายสำเร็จ');
- 
-
- 
-    
-    
-
- }
- public function district(Request $request)
- {
-      $district = Districts::get()->where('province_id',$request->select);
-   
-    $result = array();
-      $output = '<option value="">กรุณาเลือกอำเภอ</option>';
-     foreach($district as $item){
-         
-         $output .= '<option value="'.$item->id.'">'.$item->name_in_thai.'</option>';
-     }
-
-      echo $output;
-      
-     
- }
 }
-
