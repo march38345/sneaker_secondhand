710afd951a9795aea3d5fe15dd7285753cad5d54
march38345 <martz123456@hotmail.com>
2021-12-09 04:19:58 +0700

    make it better

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,6 +9,7 @@
 
     <head>
 
+<meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -166,8 +167,14 @@
 
 
     </body>
+
  
     <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
         var countblockmain = 1;
         var blockmain = document.getElementsByClassName('titlehead');
         var blocklen = blockmain.length;
