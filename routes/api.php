48cc2ef6444c3462443e526629070bb9edf992d6
march38345 <martz123456@hotmail.com>
2021-12-15 01:09:02 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware'=>['auth:sanctum']], function () {
+   Route::apiResource('order',ApiController::class);
+});
+Route::post('login',function(Request $request){
+    
+  if(auth()->attempt(array('email'=>$request->email,'password'=>$request->password))){
+    $user = User::where('email',$request->email)->first();
+    $user->tokens()->delete();
+    $token =  $user->createToken($user->name);   
+    return response()->json(['token'=>$token->plainTextToken]);
+        
+    }else{
+        abort(401);
+    }
+
+  
+});
+//Route::apiResource('order',ApiController::class);
+ 
